23d7262a118c362c1b80848e55e224c4ff191214
LongHoang <hoanglongvt97@gmail.com>
2019-08-09 10:19:38 +0700

    nothing change

--- app/src/main/java/com/vietis/bullybosschat/UserProfileAcitivity.java
+++ app/src/main/java/com/vietis/bullybosschat/UserProfileAcitivity.java
@@ -1,8 +1,8 @@
 package com.vietis.bullybosschat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class UserProfileAcitivity extends AppCompatActivity {
 
